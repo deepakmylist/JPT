af2b2fe12ea5e6d32cb45c99e3b64dbbdb071bd3
Deepak Sharma <collabalist@gmail.com>
2026-07-08 09:51:46 +0530

    providers updated

--- config.js
+++ config.js
@@ -23,6 +23,72 @@ const AppConfig = {
             name: "Vid King",
             movieUrl: "https://www.vidking.net/embed/movie/",
             tvUrl: "https://www.vidking.net/embed/tv/"
+        },
+        // --- Newly Added Providers Below ---
+        provider4: {
+            name: "Vidfast Net",
+            movieUrl: "https://vidfast.net/embed/movie/",
+            tvUrl: "https://vidfast.net/embed/tv/"
+        },
+        provider5: {
+            name: "Vidfast Pro",
+            movieUrl: "https://vidfast.pro/embed/movie/",
+            tvUrl: "https://vidfast.pro/embed/tv/"
+        },
+        provider6: {
+            name: "Vidfast In",
+            movieUrl: "https://vidfast.in/embed/movie/",
+            tvUrl: "https://vidfast.in/embed/tv/"
+        },
+        provider7: {
+            name: "Vidfast Io",
+            movieUrl: "https://vidfast.io/embed/movie/",
+            tvUrl: "https://vidfast.io/embed/tv/"
+        },
+        provider8: {
+            name: "Vidfast Pm",
+            movieUrl: "https://vidfast.pm/embed/movie/",
+            tvUrl: "https://vidfast.pm/embed/tv/"
+        },
+        provider9: {
+            name: "Vidfast Xyz",
+            movieUrl: "https://vidfast.xyz/embed/movie/",
+            tvUrl: "https://vidfast.xyz/embed/tv/"
+        },
+        provider10: {
+            name: "VID Easy Net",
+            movieUrl: "https://player.videasy.net/movie/", 
+            tvUrl: "https://player.videasy.net/tv/"       
+        },
+        provider11: {
+            name: "Vidnest Fun",
+            movieUrl: "https://vidnest.fun/embed/movie/",
+            tvUrl: "https://vidnest.fun/embed/tv/"
+        },
+        provider12: {
+            name: "111 Movies",
+            movieUrl: "https://111movies.net/movie/", 
+            tvUrl: "https://111movies.net/tv/"
+        },
+        provider13: {
+            name: "VS Embed RU",
+            movieUrl: "https://vsembed.ru/embed/movie/",
+            tvUrl: "https://vsembed.ru/embed/tv/"
+        },
+        provider14: {
+            name: "VidSrc XYZ",
+            movieUrl: "https://vidsrc.xyz/embed/movie/",
+            tvUrl: "https://vidsrc.xyz/embed/tv/"
+        },
+        provider15: {
+            name: "VidSrc Embed RU",
+            movieUrl: "https://vidsrc-embed.ru/embed/movie/",
+            tvUrl: "https://vidsrc-embed.ru/embed/tv/"
+        },
+        provider16: {
+            name: "VidSrcMe SU",
+            movieUrl: "https://vidsrcme.su/embed/movie/",
+            tvUrl: "https://vidsrcme.su/embed/tv/"
         }
     },
     
